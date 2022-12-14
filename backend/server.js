26c2758808ddf3d646c333db9e20901fa93c92f5
shagun199 <shagun199@gmail.com>
2022-12-14 18:46:39 +0530

    minor bug fix

--- backend/server.js
+++ backend/server.js
@@ -3,5 +3,5 @@ import express from "express";
 const app = express();
 
 app.listen(4000, () => {
-  console.log("Server is running on port 4000");
+  console.log("Server is running on port 4000 !!");
 });
